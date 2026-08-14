7f25c98c6dd68e00f53946cef9d9d72a1ce3f7ce
Ferdinand373 <freddycogrel@live.com>
2026-08-14 23:09:18 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,65 +1,36 @@
-const CACHE_NAME = 'mon-carnet-cuisine-v2-2-final-copy-paste-r2';
-const CORE_FILES = ['./', './index.html', './mon-carnet-v17.png'];
+const CACHE='clair-repas-v43-courses-controlees-20260814';
+const ASSETS=['./','./index.html','./manifest.webmanifest','./icon-192.png','./icon-512.png'];
 
-self.addEventListener('install', event => {
-  event.waitUntil((async () => {
-    const cache = await caches.open(CACHE_NAME);
-    for (const path of CORE_FILES) {
-      try {
-        const response = await fetch(new Request(path, { cache: 'reload' }));
-        if (response.ok) await cache.put(path, response.clone());
-      } catch (_) {}
-    }
-    await self.skipWaiting();
-  })());
+self.addEventListener('install',event=>{
+  event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(ASSETS)));
+  self.skipWaiting();
 });
 
-self.addEventListener('activate', event => {
-  event.waitUntil((async () => {
-    const names = await caches.keys();
-    await Promise.all(
-      names
-        .filter(name => name.startsWith('mon-carnet-cuisine-') && name !== CACHE_NAME)
-        .map(name => caches.delete(name))
-    );
-    await self.clients.claim();
-  })());
+self.addEventListener('activate',event=>{
+  event.waitUntil(
+    caches.keys().then(keys=>Promise.all(
+      keys.filter(k=>k.startsWith('clair-repas-')&&k!==CACHE).map(k=>caches.delete(k))
+    ))
+  );
+  self.clients.claim();
 });
 
-self.addEventListener('fetch', event => {
-  const request = event.request;
-  if (request.method !== 'GET') return;
-  const url = new URL(request.url);
-  if (url.origin !== self.location.origin) return;
-
-  if (request.mode === 'navigate') {
-    event.respondWith((async () => {
-      try {
-        const response = await fetch(request, { cache: 'no-store' });
-        if (response.ok) {
-          const cache = await caches.open(CACHE_NAME);
-          await cache.put('./index.html', response.clone());
-          await cache.put('./', response.clone());
-        }
+self.addEventListener('fetch',event=>{
+  if(event.request.mode==='navigate'){
+    event.respondWith(
+      fetch(event.request).then(response=>{
+        const copy=response.clone();
+        caches.open(CACHE).then(cache=>cache.put('./index.html',copy));
         return response;
-      } catch (_) {
-        return (await caches.match(request))
-          || (await caches.match('./index.html'))
-          || (await caches.match('./'))
-          || Response.error();
-      }
-    })());
+      }).catch(()=>caches.match('./index.html'))
+    );
     return;
   }
-
-  event.respondWith((async () => {
-    const cached = await caches.match(request);
-    if (cached) return cached;
-    const response = await fetch(request);
-    if (response.ok) {
-      const cache = await caches.open(CACHE_NAME);
-      await cache.put(request, response.clone());
-    }
-    return response;
-  })());
+  event.respondWith(
+    caches.match(event.request).then(cached=>cached||fetch(event.request).then(response=>{
+      const copy=response.clone();
+      caches.open(CACHE).then(cache=>cache.put(event.request,copy));
+      return response;
+    }))
+  );
 });
